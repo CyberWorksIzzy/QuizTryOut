a6f043cd87cda035705c9fd06e49cbab5e7f7529
CyberWorksIzzy <i.bencherif@student.fontys.nl>
2021-01-05 12:24:28 +0100

    Added + 2 to vallue

--- src/Main/TryOut.java
+++ src/Main/TryOut.java
@@ -5,6 +5,6 @@ public class TryOut {
     public static void main(String[] args) {
         // Mj schrijvf een dit comment
         int value = 1 + 3;
-        System.out.println(value);
+        System.out.println(value + 6);
     }
 }
